668e9feaadd76200ce748657851cd219fc05a7cc
mikefab <mikefabrikant@gmail.com>
2017-05-30 18:25:54 -0400

    Process multiple zip files

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var aggregate = require('./aggregate/spark_aggregate');
 var async = require('async');
 var fs = require('fs');
+var bluebird = require('bluebird');
 var exec = require('child_process').exec;
 var config = require('./config');
 var zipped = fs.readdirSync(config.zipped);
@@ -10,57 +11,75 @@ var path_processed = config.processed;
 var path_temp = config.temp;
 var util = require('./utility');
 
+bluebird.each(zipped, file => {
+  console.log('Processing', file);
+  return process_file(file);
+}, {concurreny: 1})
+.then(process.exit);
 
-async.waterfall([
+function process_file(file) {
+  return new Promise((resolve, reject) => {
+    async.waterfall([
 
-  function(callback) {
-    var command = 'rm -rf ' + path_processed + '*';
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-      }
-      callback(null)
-    });
-  },
+      function(callback) {
+        var command = 'rm -rf ' + path_processed + '*';
+        exec(command, (err, stdout, stderr) => {
+          if (err) {
+            console.error(err);
+          }
+          callback(null)
+        });
+      },
 
-  function(callback) {
-    var command = 'rm ' + path_temp + '*';
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-      }
-      callback(null)
-    });
-  },
-  function(callback) {
-    var unzipped_file = zipped[0].replace(/.gz$/, '');
-    var command = 'gunzip -c ' + config.zipped + zipped[0] + ' > ' + path_unzipped + unzipped_file;
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-      }
-      callback(null, unzipped_file)
-    });
-  },
+      function(callback) {
+        var command = 'rm ' + path_temp + '*';
+        exec(command, (err, stdout, stderr) => {
+          if (err) {
+            console.error(err);
+          }
+          callback(null)
+        });
+      },
+      function(callback) {
+        var command = 'rm ' + path_unzipped + '*';
+        exec(command, (err, stdout, stderr) => {
+          if (err) {
+            console.error(err);
+          }
+          callback(null)
+        });
+      },
+      function(callback) {
+        var unzipped_file = file.replace(/.gz$/, '');
+        var command = 'gunzip -c ' + config.zipped + file + ' > ' + path_unzipped + unzipped_file;
+        exec(command, (err, stdout, stderr) => {
+          if (err) {
+            console.error(err);
+          }
+          callback(null, unzipped_file)
+        });
+      },
 
-  function(unzipped_file, callback) {
-    aggregate.aggregate(unzipped_file, path_unzipped, path_processed)
-    .then(() => {
-      callback(null, unzipped_file)
-    })
-  },
+      function(unzipped_file, callback) {
+        aggregate.aggregate(unzipped_file, path_unzipped, path_processed)
+        .then(() => {
+          callback(null, unzipped_file)
+        })
+      },
 
-  function(callback) {
-    util.combine_spark_output(zipped[0].replace(/.gz$/, ''))
-    .catch(function(err) {
-      console.log(err);
-    })
-    .then(function() {
-      callback(null, unzipped_file);
-    });
-  },
+      function(callback) {
+        util.combine_spark_output(file.replace(/.gz$/, ''))
+        .catch(function(err) {
+          console.log(err);
+        })
+        .then(function() {
+          callback(null, unzipped_file);
+        });
+      },
 
-], () => {
-    console.log("Done aggregating!!!");
-    // callback();
-  }, 1);
+    ], () => {
+        console.log("Done aggregating!!!");
+        // callback();
+      }, 1);
+  })
+}
