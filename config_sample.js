b166a4a8e7be9b639dc476be32b86e6edafd3ff4
mikefab <mikefabrikant@gmail.com>
2017-05-30 14:42:41 -0400

    config sample

---
+++ config_sample.js
@@ -0,0 +1,8 @@
+module.exports = {
+  zipped: '../../sandboxdata/mnt/amadeus/raw/traffic/',
+  unzipped: '../../sandboxdata/mnt/traffic/unzipped/',
+  processed: '../../sandboxdata/mnt/traffic/processed/',
+  aggregated: '../../sandboxdata/aggregations/mobility/amadeus/traffic/country/',
+  temp: '../../sandboxdata/aggregations/mobility/amadeus/traffic/country/',
+  fields: ['year', 'week', 'count', 'origin', 'destination']
+}
