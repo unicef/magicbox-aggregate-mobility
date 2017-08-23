e37fccd4d0c42ecfbc62cb37dc1ab17246c911e5
mikefab <mikefabrikant@gmail.com>
2017-08-23 16:42:19 -0400

    Path to spark

--- aggregate/spark_aggregate.js
+++ aggregate/spark_aggregate.js
@@ -2,13 +2,14 @@ var util = require('util');
 var exec = require('child_process').exec;
 var aggregate = require('../aggregate/spark_aggregate');
 var child;
+var spark_path = require('../config').spark_path;
 
 // executes `pwd`
 exports.aggregate = function(file, path_unzipped, path_temp) {
 
   console.log('SPARK CLI!', file);
   return new Promise(function(resolve, reject) {
-    var command = 'spark-shell -i ./spark/aggregate.scala --conf spark.driver.extraJavaOptions="-D' + file + ',' + path_unzipped + ',' + path_temp +  '"'
+    var command = spark_path + 'spark-shell -i ./spark/aggregate.scala --conf spark.driver.extraJavaOptions="-D' + file + ',' + path_unzipped + ',' + path_temp +  '"'
 
     child = exec(
       command, function (error, stdout, stderr) {
