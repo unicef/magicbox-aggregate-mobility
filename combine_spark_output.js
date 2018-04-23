0e57070f32df809afc3b008a94f109411fa33818
Joachim Chapman <jc@quack.zone>
2018-04-23 22:45:12 +0100

    build failed because it looks like it had trouble creating the output file (or path). This probably won't fix the problem but should give more information if it happens again. Errors are written to the console but not bubbled up. I think this retains the existing behaviour - if a file fails, move on to the next.

--- combine_spark_output.js
+++ combine_spark_output.js
@@ -10,7 +10,7 @@ const path = require('path')
  * Combines spark output to files by week date
  * @param{String} file_named_dir - name of CSV that spark uses to create dir to store output
  * @param{String} path_temp - directory where spark stores output
- * @param{String} path_summarized - diretory to store summarized travel by week
+ * @param{String} path_summarized - directory to store summarized travel by week
  * @param{Object} date_lookup - object that maps a year to a specific date 'YYYY-MM-DD'
  * @return{Promise} Fulfilled when records are returned
  */
@@ -36,10 +36,10 @@ exports.combine_spark_output = (
         date_lookup
       )
     }).catch(reject)
-    .then(() => {
-      console.log('done all files')
-      resolve()
-    })
+      .then(() => {
+        console.log('done all files')
+        resolve()
+      })
   })
 }
 
@@ -49,7 +49,7 @@ exports.combine_spark_output = (
  * @param{String} f - file output by spark - part-r-00000-78d8da1e-4612-40fb-ad03-3eacfc0214d6.csv
  * @param{String} file_named_dir - name of CSV that spark uses to create dir to store output
  * @param{String} path_temp - directory where spark stores output
- * @param{String} path_summarized - diretory to store summarized travel by week
+ * @param{String} path_summarized - directory to store summarized travel by week
  * @param{Object} date_lookup - object that maps a year to a specific date 'YYYY-MM-DD'
  * @return{Promise} Fulfilled when records are returned
  */
@@ -87,7 +87,7 @@ const process_file = (
     bluebird.each(Object.keys(records), week => {
       let year = records[week][0].year
       let date = moment(date_lookup[year])
-        .add(week -1, 'weeks')
+        .add(week - 1, 'weeks')
         .format('YYYY-MM-DD')
       return create_or_append(path_summarized + date + '.csv', records[week])
     }).catch(console.log).then(resolve)
@@ -95,28 +95,54 @@ const process_file = (
 }
 
 /**
+ * ensure path for output file exists.
+ * @param{string} output_file_path path to the file to create directories for
+ * @throws{object} error from mkdirp
+ */
+const make_directory_for_file = (output_file_path) => {
+  const directory_name = path.dirname(output_file_path)
+  let create_directory_error = null
+  mkdirp(directory_name, (error) => {
+    create_directory_error = error
+  })
+  if (create_directory_error) {
+    throw create_directory_error
+  } else {
+    console.log('created directory', directory_name, 'for', output_file_path)
+  }
+}
+
+/**
  * Create or append travel by file titled with date of week
- * @param{String} outputFilePath - path to directory where records summarized by week are stored.
+ * @param{String} output_file_path - path to directory where records summarized by week are stored.
  * @param{Array} week_ary - array of records per that week
  * @return{Promise} Fulfilled when records are returned
  */
-const create_or_append = (outputFilePath, week_ary) => {
-  console.log(outputFilePath, '!!!!')
+const create_or_append = (output_file_path, week_ary) => {
+  console.log(output_file_path, '!!!!')
   let csv = week_ary.reduce((s, d) => {
     s += [d.origin, d.destination, d.count] + '\n'
     return s
   }, '')
 
   return new Promise((resolve, reject) => {
-    fs.exists(outputFilePath, exists => {
-      if (exists) {
-        fs.appendFileSync(outputFilePath, csv + '\n')
-      } else {
-        mkdirp(path.dirname(outputFilePath))
-        fs.writeFileSync(outputFilePath, 'orig, dest, cnt' + '\n')
-        fs.appendFileSync(outputFilePath, csv + '\n')
-      }
-      resolve()
-    })
+    try {
+      fs.exists(output_file_path, exists => {
+        try {
+          if (exists) {
+            fs.appendFileSync(output_file_path, csv + '\n')
+          } else {
+            make_directory_for_file(output_file_path)
+            fs.writeFileSync(output_file_path, 'orig, dest, cnt' + '\n')
+            fs.appendFileSync(output_file_path, csv + '\n')
+          }
+        } catch (error) {
+          console.error(error)
+        }
+      })
+    } catch (error) {
+      console.error(error)
+    }
+    resolve()
   })
 }
